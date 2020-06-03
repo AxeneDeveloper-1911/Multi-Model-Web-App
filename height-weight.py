f5628cda27c5e29a86efcedc158671d5a6cfce28
AxeneDeveloper-1911 <62709242+AxeneDeveloper-1911@users.noreply.github.com>
2020-06-03 16:29:56 +0530

    Fixed the issues
    Having the APIs in different files was causing the issue.
    Now just run this file and make sure that the home.html have an anchor tag with href="/predict"

--- height-weight.py
+++ height-weight.py
@@ -1,25 +1,31 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
+from app import app
 
+
+model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 
 @app.route('/predict')
 def predict():
     return render_template('height-weight.html')
 
 
-@app.route('/predict_weight',methods=['GET', 'POST'])
+@app.route('/predict',methods=['POST'])
 def predict_weight():
-    if request.method == 'POST':
-        height = float(request.form['height'])
-        gender = float(request.form['gender'])
-        
-        prediction = model.predict([[gender, height]])
-        
-        output = round(prediction[0], 2)
+    height = float(request.form['height'])
+    gender = float(request.form['gender'])
+
+    prediction = model.predict([[gender, height]])
+
+    output = round(prediction[0], 2)
         
     return render_template('height-weight.html', weights = output)
     
